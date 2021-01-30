a8b63632316af20860a877d4608404a07e4e90d6
Dagimawi D. Eneyew <deneyew@uwo.ca>
2021-01-30 00:22:03 -0500

    Select folder function implemented
    Select folder function that opens the file explorer and grabs the path, then assigns it to a gui label.

---
+++ Main.py
@@ -0,0 +1,18 @@
+#importing tkinter module
+from tkinter import *
+# importing filedialog module 
+from tkinter import filedialog 
+
+
+#function to explore and select folder
+def selectFolder(): 
+    '''Function to selct folder from file explorer
+    Parameters: empty
+    Returns:
+    void
+    '''
+    #open file explorer and grab the path
+    folder_selected = filedialog.askdirectory() 
+    #Assign the path to text label 
+    path_Label.configure(text="Folder Selected: "+folder_selected) 
+       
