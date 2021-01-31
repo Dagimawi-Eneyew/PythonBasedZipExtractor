a0c5e3863fe7765073616b95b9a5c9ffa1b13832
Dagimawi D. Eneyew <deneyew@uwo.ca>
2021-01-30 20:49:47 -0500

    GUI elements updated and code formatted
    GUI elements updated.

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 #importing tkinter module
 from tkinter import *
-# importing filedialog module 
-from tkinter import filedialog 
+# importing filedialog module
+from tkinter import filedialog
 #import scrolled text
 from tkinter import scrolledtext
-import zipfile,fnmatch,os
+import zipfile
+import fnmatch
+import os
 import traceback
 
 #global variable to hold the path
 global folder_selected
-folder_selected=''
+folder_selected = ''
 
 #Function to extract zip files
+
+
 def extractZIP(rootPath):
     '''Function to traverse directory and extract zip files
     Parameters: rootPath (String)
@@ -19,31 +23,37 @@ def extractZIP(rootPath):
     void
     '''
     text_area.configure(state='normal')
-    text_area.insert("end",folder_selected)
+    text_area.insert("end", folder_selected)
     try:
         pattern = '*.zip'
         for root, dirs, files in os.walk(rootPath):
             for filename in fnmatch.filter(files, pattern):
-                output=os.path.join(root, filename)+'  Extracted...'
-                zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
+                output = os.path.join(root, filename)+'  Extracted...'
+                zipfile.ZipFile(os.path.join(root, filename)).extractall(
+                    os.path.join(root, os.path.splitext(filename)[0]))
                 zipped_file = os.path.join(root, filename)
                 os.remove(zipped_file)
                 text_area.configure(state='normal')
-                text_area.insert("end",output+'\n\n')
+                text_area.insert("end", output+'\n\n')
                 text_area.configure(state='disabled')
-                
+
     except Exception as e:
         text_area.configure(state='normal')
-        text_area.insert("end","\n------------------------------------------------------------------------------------------------\n")
-        text_area.insert("end","                               Console Log (Error Report)\n")
-        text_area.insert("end","------------------------------------------------------------------------------------------------\n")
-        text_area.insert("end",str(traceback.format_exc())+"\n",'error')
+        text_area.insert(
+            "end", "\n------------------------------------------------------------------------------------------------\n")
+        text_area.insert(
+            "end", "                               Console Log (Error Report)\n")
+        text_area.insert(
+            "end", "------------------------------------------------------------------------------------------------\n")
+        text_area.insert("end", str(traceback.format_exc())+"\n", 'error')
         text_area.tag_config('error', foreground='red')
         text_area.configure(state='disabled')
-    finally: 
+    finally:
         text_area.yview(END)
 
 #Function to display information about the application
+
+
 def messageDisplay():
     '''Function to selct folder from file explorer
         Parameters: empty
@@ -51,78 +61,95 @@ def messageDisplay():
         void
         '''
     text_area.configure(state='normal')
-    text_area.insert("end","\n------------------------------------------------------------------------------------------------\n")
-    text_area.insert("end","                               About The Application \n")
-    text_area.insert("end","------------------------------------------------------------------------------------------------\n")
-    text_area.insert("end","ZipExtractor is a free license software originally developed for personal use. \nThis software can extract zip files by traversing each directory.\n Developer : Dagimawi Eneyew \n Verison: 1.0.0 \n")
+    text_area.insert(
+        "end", "\n------------------------------------------------------------------------------------------------\n")
+    text_area.insert(
+        "end", "                               About The Application \n")
+    text_area.insert(
+        "end", "------------------------------------------------------------------------------------------------\n")
+    text_area.insert("end", "ZipExtractor is a free license software originally developed for personal use. \nThis software can extract zip files by traversing each directory.\n Developer : Dagimawi Eneyew \n Verison: 1.0.0 \n")
     text_area.configure(state='disabled')
     text_area.yview(END)
 
 
-
-
 #function to explore and select folder
-def selectFolder(): 
+def selectFolder():
     '''Function to selct folder from file explorer
     Parameters: empty
     Returns:
     void
     '''
     global folder_selected
-    folder_selected = filedialog.askdirectory()  
-    # Change label contents 
-    folder_selected=folder_selected.replace('/',"\\")
+    folder_selected = filedialog.askdirectory()
+    # Change label contents
+    folder_selected = folder_selected.replace('/', "\\")
     #folder_selected=folder_selected.replace('\\',"\\\\")
-    label_file_explorer.configure(text="Folder Selected: "+folder_selected)  
+    label_file_explorer.configure(text="Folder Selected: "+folder_selected)
 
-#Function to close the application window     
-def close_window (): 
+#Function to close the application window
+
+
+def close_window():
     '''Function to close the application window
     Parameters: empty
     Returns:
     void
     '''
-    window.destroy()   
+    window.destroy()
 
 
 # Create GUI elements
-#label to hold the path string
-path_Label = Label(window, text = "Folder not selected !", width = 100, height = 4,  fg = "blue") 
-   
-#explore window button  
-explore_Button = Button(window,  text = "Browse Files", command = browseFiles)  
+# Create the root window
+window = Tk()
 
-#application exit button  
-exit_Button = Button(window,  text = "Exit", command = close_window)  
+# Set window title
+window.title('ZIp File Extractor')
 
-#Text are to display the logs
-text_area = scrolledtext.ScrolledText(window, width = 80,  height = 20) 
+# Set window size
+window.geometry("840x500")
 
-#progress Log title 
-progress_Log = Label(window,  text = "Progress Log...", width = 60, height = 4,  fg = "black",font = ("Times New Roman", 
-                                              15)) 
 
-#creating the root window
-window = Tk() 
+text_area = scrolledtext.ScrolledText(window,
+                                      width=100,
+                                      height=20)
 
-# Window title 
-window.title('Zip File Extractor') 
-   
-# Set window size 
-window.geometry("700x500") 
-   
-#Set window background color 
-window.config(background = "white") 
+text_area.grid(column=0, pady=10, padx=10)
 
-#Adjust the location of GUI elements
-path_Label.grid(column = 1, row = 1) 
-explore_Button.grid(column = 1, row = 2) 
-progress_Log.grid(column=1,row=3)
-text_area.grid(column=1,row=4)
-button_exit.grid(column = 1,row = 8) 
 
-#Make the window unresizable
-window.resizable(width=0,height=0)
+# Making the text read only
+#text_area.configure(state ='disabled')
 
-#Window main loop 
-window.mainloop() 
+#Set window background color
+window.config(background="indigo")
+
+# Create a File Explorer label
+label_file_explorer = Label(window,
+                            text="Folder not selected",
+                            width=80, height=2,
+                            fg="white", background="indigo", font=("Times New Roman",
+                                                                   12), anchor='w')
+#label to display progress log
+progress_Log = Label(window,
+                     text="Progress Log...",
+                     width=80, height=1,
+                     fg="white", font=("Times New Roman",
+                                       14), background="indigo", anchor='w')
+#A button to open explorer
+button_explore = Button(window, width=15,
+                        text="Browse Files",
+                        command=selectFolder)
+#A button to display about info
+button_about = Button(window, width=15,
+                      text="About", command=messageDisplay)
+#A button to initiate extract operation
+button_extract = Button(window, width=15, text="Extract Files",
+                        command=lambda: extractZIP(folder_selected))
+
+#A button to close the application
+button_exit = Button(window,  width=12,
+                     text="Exit",
+                     command=close_window)
+
+
+#Window main loop
+window.mainloop()
